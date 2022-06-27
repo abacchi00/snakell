521c58b88303377347235b4bbc6f03ea14026ebf
abacchi00 <andre.rossini.bacchi@gmail.com>
2022-06-27 01:04:22 -0300

    Make some progress

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,18 @@
 import Graphics.Gloss
 import Graphics.Gloss.Data.ViewPort
 import Graphics.Gloss.Interface.Pure.Game
+import Data.Fixed
+import System.Random
+
+truncate' :: Float -> Int -> Float
+truncate' x n = (fromIntegral (floor (x * t))) / t
+  where t = 10 ^ n
+
+pseudoRandomNumber :: Float -> Float
+pseudoRandomNumber seed = (take 1 . randomRs ((-9), 9) . mkStdGen $ round seed) !! 0
+
+pseudoRandomCoordinate :: Float -> Float -> (Float, Float)
+pseudoRandomCoordinate x y = (((pseudoRandomNumber x) `truncate'` 0) * 20, ((pseudoRandomNumber y) `truncate'` 0) * 20)
 
 width, height, offset :: Int
 width = 400
@@ -19,28 +31,34 @@ background = black
 
 data SnakellGame = Game
   { snakeHeadLoc :: (Float, Float) -- Snake's head (x, y) location
+  , snakeTailLoc :: [(Float, Float)] -- Snake's tail blocks (x, y) location
   , snakeVel :: (Float, Float) -- Pixels por frame, como os blocos do jogo são 20 x 20, a cobra anda de 20 em 20 pixels
+  , appleLoc :: (Float, Float) -- Apple's (x, y) location
   } deriving Show
 
 initialState :: SnakellGame
 initialState = Game
-  { snakeHeadLoc = (100, 100)
+  { snakeHeadLoc = (100, 0)
+  , snakeTailLoc = [(100, 20), (100, 40), (100, 60)]
   , snakeVel = (0, -20) -- (0, -20) é pra baixo, (0, 20) é pra cima, (-20, 0) é pra esquerda, (20, 0) é pra direita, 
+  , appleLoc = (-60, 60)
   }
 
 render :: SnakellGame -> Picture
 render game =
-  pictures [snakeHead, snakeTail, walls]
+  pictures [walls, apple, snakeHead, snakeTail]
 
   where
     -- Snake
-    snakeHead = uncurry translate headPosition $ color snakeColor $ rectangleSolid 20 20
+    snakeHead = uncurry translate headPosition $ color (dark (dark green)) $ rectangleSolid 20 20
 
-    snakeTail = uncurry translate tailPosition $ color blue $ rectangleSolid 20 20
+    snakeTail = pictures [uncurry translate tailBlockPosition $ color snakeColor $ circleSolid 10 | tailBlockPosition <- tailPositions ]
 
-    tailPosition = (fst headPosition - fst vel, snd headPosition - snd vel)
+    apple = uncurry translate (appleLoc game) $ color red $ rectangleSolid 20 20
+
     headPosition = snakeHeadLoc game
-    vel = snakeVel game
+    tailPositions = snakeTailLoc game
+    
 
     -- Walls
     horizontalWall, verticalWall :: Float -> Picture
@@ -61,7 +79,7 @@ render game =
     wallColor = greyN 0.5
 
 moveSnake :: Float -> SnakellGame -> SnakellGame
-moveSnake seconds game = game { snakeHeadLoc = (x', y') }
+moveSnake seconds game = game { snakeHeadLoc = (x', y'), snakeTailLoc = newTailPositions (snakeTailLoc game), appleLoc = newAppleLoc }
   where
     -- Localização e velocidade antiga
     (x, y) = snakeHeadLoc game
@@ -73,6 +91,15 @@ moveSnake seconds game = game { snakeHeadLoc = (x', y') }
     -- x' = x + vx * seconds
     -- y' = y + vy * seconds
 
+    teste _ [] = []
+    teste _ [a] = if (snakeEatsApple) then [a] else []
+    teste a (_:xs) = [a] ++ teste (head xs) xs
+    newTailPositions xs = [(x, y)] ++ teste (head xs) xs
+
+    newAppleLoc :: (Float, Float)
+    newAppleLoc = if snakeEatsApple then pseudoRandomCoordinate (x + 79) (y + 32) else appleLoc game
+
+    snakeEatsApple = snakeHeadLoc game == appleLoc game
 
 -- | Respond to key events.
 handleKeys :: Event -> SnakellGame -> SnakellGame
@@ -100,7 +127,7 @@ handleKeys _ game = game
 -- Numero de frames para mostrar por segundo
 fps :: Int
 -- fps = 60
-fps = 5
+fps = 10
 
 main :: IO ()
 main = play window background fps initialState render handleKeys update
