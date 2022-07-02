34bb08b97a87aad3e3b80e881348af1dab948f9c
abacchi00 <andre.rossini.bacchi@gmail.com>
2022-07-02 17:47:02 -0300

    Add bullet / apple collision / player score

--- src/Main.hs
+++ src/Main.hs
@@ -19,6 +19,14 @@ blockSize = blockSizeConfig "normal" -- "normal" (default), "small" or "smaller"
 windowSize = blockSize * 40.0
 snakeTailSpacing = tailSpacingConfig "none" -- "none" (default), "some" or "separate"
 
+-- Helpers
+
+windowRadius  = windowSize / 2
+blockRadius   = blockSize / 2
+defaultCircle = circleSolid blockRadius
+
+--
+
 blockSizeConfig :: [Char] -> Float
 blockSizeConfig s
   | s == "normal" = 20.0 
@@ -45,13 +53,14 @@ truncate' :: Float -> Int -> Float
 truncate' x n = (fromIntegral (floor (x * t))) / t
   where t = 10 ^ n
 
--- TODO Random positions basend on windowSize (some configs doesnt work properly)
-pseudoRandomNumber :: Float -> Float
-pseudoRandomNumber seed = (take 1 . randomRs ((-19), 19) . mkStdGen $ round seed) !! 0
+-- TODO Random positions based on windowSize (some configs doesnt work properly)
+pseudoRandomNumber :: Float -> Float -> Float -> Float
+pseudoRandomNumber seed r1 r2 = (take 1 . randomRs (r1, r2) . mkStdGen $ round seed) !! 0
 
 pseudoRandomCoordinate :: Float -> Float -> (Float, Float)
-pseudoRandomCoordinate x y = (((pseudoRandomNumber x) `truncate'` 0) * 20, ((pseudoRandomNumber y) `truncate'` 0) * 20)
-
+pseudoRandomCoordinate x y = (pseudoRandomNumber x (-r) r, pseudoRandomNumber y (-r) r)
+  where
+    r = windowRadius - blockSize
 width, height, offset :: Int
 width = toInt windowSize
 height = toInt windowSize
@@ -66,12 +75,15 @@ background = black
 type Coordinate = (Float, Float)
 type SnakeDirection = Coordinate  -- Snake's directon can have four values: (0, -1) = down | (0, 1) = up | (-1, 0) = left | (1, 0) = right
 type SnakeBlock = (Coordinate, SnakeDirection) -- Snake's block (x, y) coordinate and (a, b) direction
+type BulletState = (Coordinate, SnakeDirection) -- Snake's block (x, y) coordinate and (a, b) direction
 
 data SnakellGame = Game
   { snakeHeadLoc :: Coordinate -- Snake's head (x, y) location
   , snakeTailLoc :: [SnakeBlock] -- Snake's tail blocks (x, y) location
   , snakeDirection :: SnakeDirection -- Snake's directon: (0, -1) = down | (0, 1) = up | (-1, 0) = left | (1, 0) = right, 
   , appleLoc :: Coordinate -- Apple's (x, y) location
+  , bullet :: BulletState
+  , playerScore :: Float
   } deriving Show
 
 wallCollision :: Coordinate -> Float -> [Char] 
@@ -98,7 +110,10 @@ initialHeadLoc :: Coordinate
 initialHeadLoc = (blockSize * 5, 0)
 
 initialSnakeDirection :: SnakeDirection
-initialSnakeDirection = (0, -1) 
+initialSnakeDirection = (0, -1)
+
+restingBulletState :: BulletState
+restingBulletState = ((500, 500), (0, 0))
 
 initialSnakeTail :: [SnakeBlock]
 initialSnakeTail = firstIncrement ++ secondIncrement ++ thirdIncrement
@@ -113,40 +128,76 @@ initialState = Game
   , snakeTailLoc = initialSnakeTail
   , snakeDirection = initialSnakeDirection
   , appleLoc = (-(blockSize * 3), (blockSize * 3))
+  , bullet = restingBulletState
+  , playerScore = 0
   }
 
 render :: SnakellGame -> [Picture] -> Picture
 render game imgs =
-  pictures [apple, snakeTail, snakeHead, walls]
+  pictures [gameGrass, apple, snakeTail, bulletPic, snakeHead, walls, gameScore]
 
   where
     -- Snake
-    snakeHead = uncurry translate headPosition $ color (dark (dark green)) $ circleSolid (blockSize / 2)
+    snakeHead :: Picture
+    snakeHead = uncurry translate headPosition $ rotate headRotation (scale 0.08 0.08 snakeHeadPic)
+      where
+        headRotation
+          | snakeDirection game == (0, -1) = 0
+          | snakeDirection game == (0, 1)  = 180
+          | snakeDirection game == (-1, 0) = 90
+          | snakeDirection game == (1, 0)  = 270
 
-    snakeTail = pictures [uncurry translate (fst tailBlockPosition) $ head imgs | tailBlockPosition <- tailPositions]
+    snakeHeadPic = imgs !! 2
 
-    apple = uncurry translate (appleLoc game) $ color red $ rectangleSolid blockSize blockSize
+    snakeTail :: Picture
+    snakeTail = pictures [uncurry translate (fst tailBlock) $ snakeBlockPic | tailBlock <- snakeTailLoc game]
 
+    snakeBlockPic = scale 0.04 0.04 (imgs !! 3)
+
+    apple :: Picture
+    apple = uncurry translate applePosition $ scale 2 2 applePic
+      where
+        applePic = imgs !! 1
+
+    headPosition :: Coordinate
     headPosition = snakeHeadLoc game
-    tailPositions = snakeTailLoc game
+    tailBlocks :: [SnakeBlock]
+    tailBlocks = snakeTailLoc game
+    applePosition :: Coordinate
+    applePosition = appleLoc game
+
+    -- Text
+
+    gameScore = uncurry translate (-380, -380) $ scale 0.2 0.2 $ text ("Score: " ++ (show (playerScore game)))
+
+    -- Bullet
+
+    bulletPic = pictures [
+      uncurry translate (fst (bullet game)) $ color black $ circleSolid (blockRadius / 1.4),
+      uncurry translate (fst (bullet game)) $ color (light blue) $ circleSolid (blockRadius / 2)
+      ]
 
     -- Walls
-    horizontalWall, verticalWall :: Float -> Picture
-    horizontalWall offset =
-      translate 0 offset $
-        color wallColor $
-          rectangleSolid windowSize blockSize
+    wall :: Int -> Int -> Picture
+    wall x y = translate offsetX offsetY $ wallColor $ shape
+      where
+        offsetX = if x == 1 then 0 else offset
+        offsetY = if x == 0 then 0 else offset
+        offset  = if y == 1 then windowRadius else (-windowRadius)
+        shape   = if x == 1 then rectangleSolid windowSize blockSize else rectangleSolid blockSize windowSize
+    
+    walls = pictures [wall 1 1, wall 1 0, wall 0 0, wall 0 1]
 
-    verticalWall offset =
-      translate offset 0 $
-        color wallColor $
-          rectangleSolid blockSize windowSize
-
-    walls = pictures [horizontalWall (windowSize / 2), horizontalWall (-(windowSize / 2)), verticalWall (windowSize / 2), verticalWall (-(windowSize / 2))]
-
+    -- Grass
+    grassPic = imgs !! 0
+    gameGrass = pictures [uncurry translate (x, y) $ grassPic | x <- grassPositions, y <- grassPositions]
+      where
+        grassPositions = [-windowRadius, -windowRadius + 80..windowRadius]
+    
     -- Colors
-    snakeColor = green
-    wallColor = greyN 0.5
+    snakeHeadColor = color (dark (dark (dark green)))
+    snakeTailColor = color (dark green)
+    wallColor = color $ greyN 0.5
 
 snakeIncrement :: SnakeBlock -> [SnakeBlock]
 snakeIncrement lastBlock = [lastBlock | t <- [1..snakeIncrementQuantity]]
@@ -154,7 +205,7 @@ snakeIncrement lastBlock = [lastBlock | t <- [1..snakeIncrementQuantity]]
     snakeIncrementQuantity = blockSize / snakeTailSpacing
 
 moveSnake :: Float -> SnakellGame -> SnakellGame
-moveSnake seconds game = game { snakeHeadLoc = (x'', y''), snakeTailLoc = newTailPositions (snakeTailLoc game), appleLoc = newAppleLoc }
+moveSnake seconds game = game { snakeHeadLoc = (x'', y''), snakeTailLoc = newTailPositions (snakeTailLoc game), appleLoc = newAppleLoc, bullet = nextBulletState, playerScore = newScore }
   where
     -- Localização e direção antiga
     (x, y) = snakeHeadLoc game
@@ -166,8 +217,17 @@ moveSnake seconds game = game { snakeHeadLoc = (x'', y''), snakeTailLoc = newTai
     -- x' = x + vx * seconds
     -- y' = y + vy * seconds
 
-    collision = wallCollision (x', y') (blockSize / 2)
+    collision = wallCollision (x', y') blockRadius
     (x'', y'') = newPosIfCollision collision (x', y')
+
+    -- New score
+
+    newScore :: Float
+    newScore = (playerScore) game + scoreIncrement
+      where
+        scoreIncrement = if snakeEatsApple then 3 else 0
+
+    --
 
     teste :: SnakeBlock -> [SnakeBlock] -> [SnakeBlock]
     teste _ [] = []
@@ -181,10 +241,20 @@ moveSnake seconds game = game { snakeHeadLoc = (x'', y''), snakeTailLoc = newTai
     newAppleLoc = if snakeEatsApple then genRandomAppleLoc x y else appleLoc game
 
     genRandomAppleLoc :: Float -> Float -> Coordinate
-    genRandomAppleLoc x y = pseudoRandomCoordinate (x + 11) (y + 88)
+    genRandomAppleLoc x y = pseudoRandomCoordinate seedX seedY
+      where
+        seedX = x + 11 + snd (fst (last (snakeTailLoc game)))
+        seedY = y + 88 + fst (fst (last (snakeTailLoc game)))
 
     snakeEatsApple :: Bool
-    snakeEatsApple = appleCollision (appleLoc game) (x', y') (blockSize / 2)
+    snakeEatsApple = appleCollision (appleLoc game) (x', y') blockRadius
+
+    nextBulletState = if bulletWallCollision /= "none" then restingBulletState else moveBullet
+      where
+        moveBullet = ((bulletX + bulletDirX * 10, bulletY + bulletDirY * 10), (bulletDirX, bulletDirY))
+        ((bulletX, bulletY), (bulletDirX, bulletDirY)) = bullet game
+        bulletWallCollision = wallCollision (bulletX, bulletY) (blockRadius / 2)
+
 
 -- Respond to key events.
 handleKeys :: Event -> SnakellGame -> SnakellGame
@@ -192,6 +262,11 @@ handleKeys :: Event -> SnakellGame -> SnakellGame
 -- For an 'r' keypress, reset the snake's head to the center.
 handleKeys (EventKey (Char 'r') _ _ _) game =
   game { snakeHeadLoc = (-180, 180) }
+
+-- For an 's' keypress, shoot bullet
+handleKeys (EventKey (Char 's') _ _ _) game =
+  game { bullet = if bullet game /= restingBulletState then bullet game else (snakeHeadLoc game, snakeDirection game) }
+
 
 handleKeys (EventKey (SpecialKey KeyUp) _ _ _) game =
   game { snakeDirection = (0, 1) }
@@ -220,7 +295,10 @@ update seconds game = moveSnake seconds game
 
 main :: IO ()
 main = do
+  applePicture <- loadBMP "src/apple.bmp"
+  grassPicture <- loadBMP "src/grass.bmp"
+  snakeHeadPicture <- loadBMP "src/snakeHead.bmp"
   snakeBlockPicture <- loadBMP "src/snakeBlock.bmp"
 
-  play window background fps initialState (`render` [snakeBlockPicture]) handleKeys update
+  play window background fps initialState (`render` [grassPicture, applePicture, snakeHeadPicture, snakeBlockPicture]) handleKeys update
 
